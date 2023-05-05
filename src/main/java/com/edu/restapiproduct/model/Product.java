a4c257a9754950ad967116a1f3ff3acb03a89553
DKichukov <dzhamal.kichukov@gmail.com>
2023-05-05 21:52:18 +0300

    Initial commit

---
+++ src/main/java/com/edu/restapiproduct/model/Product.java
@@ -0,0 +1,78 @@
+package com.edu.restapiproduct.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private double price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, double price, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                '}';
+    }
+}
